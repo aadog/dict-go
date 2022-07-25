22789cd39a6c0471caeaf3a378ab097024f0eb4f
g.ph <g@google.com>
2022-07-25 21:56:02 +0800

    update

--- dict_test.go
+++ dict_test.go
@@ -6,12 +6,10 @@ import (
 )
 
 func TestDict(t *testing.T) {
-	//d := Dict{}
-	//d.Set("b", "xxx")
-	//d.Set("a", 22.343)
-	//fmt.Println(d.GetString("a"))
 	d := NewDictWithMap(map[string]any{
 		"a": map[string]any{"a": "c"},
 	})
+	d.Set("b", "xxx")
+	d.Set("a", 22.343)
 	fmt.Println(d.GetAny("a"))
 }
